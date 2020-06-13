8453d13d1147cca040fb6e390f22e7c4acb2a9cf
fussel178 <fussel178@users.noreply.github.com>
2020-06-13 01:47:47 +0000

    deploy: c596d7d77c27b92ec39e21b1787493d714e27f76

---
+++ precache-manifest.5c6c496fba8c833672c13f43d863e7b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "321c97f8c24f5c6d4cec20ab09254a85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c775b1640e07fcce377",
+    "url": "/static/css/2.a1b7a17e.chunk.css"
+  },
+  {
+    "revision": "c090541318f5182d58ba",
+    "url": "/static/css/main.24e5e576.chunk.css"
+  },
+  {
+    "revision": "1c775b1640e07fcce377",
+    "url": "/static/js/2.3a94e57b.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.3a94e57b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c090541318f5182d58ba",
+    "url": "/static/js/main.d87a08c9.chunk.js"
+  },
+  {
+    "revision": "49c6a60a60cccb936720",
+    "url": "/static/js/runtime-main.d8aa63ae.js"
+  }
+]);
